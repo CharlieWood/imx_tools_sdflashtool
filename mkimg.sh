a09aad5021ea15c30fdb763dc670edfbc19e45b7
TongBo <btong@eee168.com>
2011-01-24 20:55:27 +0800

    use full path in build script
    Change-Id: I674d3a7daa09e6b21ed1b07bbc30e85760728553

--- mkimg.sh
+++ mkimg.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-bin/arm-none-linux-gnueabi-mkimage \
+$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-mkimage \
 -A arm -O linux -T ramdisk -C none -a 0x90308000 \
 -n "Android Root Filesystem" \
 -d ramdisk.img uramdisk.img && rm ramdisk.img
