74c4db068c32198dffc347e41c688613fc06c0ac
TongBo <btong@eee168.com>
2011-01-31 19:54:13 +0800

    add $(hide) to build command
    Change-Id: I7903cd1acb78ac6e3351554003ec26dbf2d54bc4

--- Android.mk
+++ Android.mk
@@ -5,10 +5,10 @@ flashramdisk: FLASH_RAMDISK_ROOT := $(LOCAL_PATH)
 flashramdisk: $(PRODUCT_OUT)/uramdisk-flash.img
 
 $(PRODUCT_OUT)/uramdisk-flash.img: | $(ACP)
-	$(FLASH_RAMDISK_ROOT)/bin/mkbootfs $(FLASH_RAMDISK_ROOT)/root > \
+	$(hide) $(FLASH_RAMDISK_ROOT)/bin/mkbootfs $(FLASH_RAMDISK_ROOT)/root > \
 		$(PRODUCT_OUT)/ramdisk-flash.img
 	@echo "Install: $@"
-	$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)mkimage \
+	$(hide) $(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)mkimage \
 		-A arm -O linux -T ramdisk -C none -a 0x90308000 \
 		-n "Android Flash Filesystem" \
 		-d "$(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/ramdisk-flash.img" \
