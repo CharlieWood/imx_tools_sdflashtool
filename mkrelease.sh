d802c28054f56e1b91ff30eea18011eb5edb6381
TongBo <btong@eee168.com>
2011-02-15 09:27:03 +0800

    copy u-boot.img to update/boot/$(boardname)_u-boot.img
    Change-Id: I2c8154cedfd8a8701ac12ecf7c5d5881bc031860

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-images="u-boot-no-padding.bin uImage uramdisk.img \
+flash_img="uImage uramdisk.img \
 	uImage-recovery uramdisk-recovery.img system.img userdata.img"
+uboot_img="u-boot-no-padding.bin"
+
+images="$uboot_img $flash_img"
 
 #usage <no_arg>
 usage()
 {
-	echo "usage: `basename $0` [-s src_directory] [-o out_dir] [-l] [-i]"
-	echo "   -s: source directory, if omit, use environment variable ANDROID_PRODUCT_OUT"
-	echo "   -o: output directory, if omit, use current work directory"
+	echo "usage: `basename $0` -s src_directory -o out_dir -b board_name [-l] [-i]"
+	echo "   -s: source directory"
+	echo "   -o: output directory"
+	echo "   -b: board name"
 	echo "   -l: use ln instead of cp to copy image files"
 	echo "   -i: create initialize release instead of update release"
 	echo "       initialize release create new partition on target device"
@@ -23,19 +27,30 @@ err_exit()
 	exit 1
 }
 
+usage_err()
+{
+	echo "Error: $1"
+	usage
+	exit 1
+}
+
 srcdir=""
 outdir=""
+boardname=""
 CPCMD="cp"
 rundir=`dirname "$0"`
 initrel=0
 
-while getopts 's:o:lih' OPT; do
+while getopts 's:o:b:lih' OPT; do
 	case $OPT in
 		s)
 			srcdir="$OPTARG"
 			;;
 		o)
 			outdir="$OPTARG"
+			;;
+		b)
+			boardname="$OPTARG"
 			;;
 		l)
 			CPCMD="ln"
@@ -50,22 +65,15 @@ while getopts 's:o:lih' OPT; do
 	esac
 done
 
-if [ -z "$srcdir" ]; then
-	srcdir="$ANDROID_PRODUCT_OUT"
-fi
-
-test -z "$srcdir" && err_exit "no source directory"
-
-if [ -z "$outdir" ]; then
-	outdir=`pwd`
-fi
-
-test ! -d "$outdir" && err_exit "invalid output directory"
+test -z "$srcdir" && usage_err "no source directory"
+test ! -d "$outdir" && usage_err "invalid output directory"
+test -z "$boardname" && usage_err "no board name"
 
 if [ ! -f "$srcdir/uramdisk-flash.img" ]; then
 	err_exit "can't found flash uramdisk in source directory"
 fi
 
+echo "checking source images ..."
 for i in $images; do
 	test ! -f "$srcdir/$i" && err_exit "image $i not found in directory '$srcdir'"
 done
@@ -75,22 +83,26 @@ test -d "$dest" && err_exit "$dest directory exist, stop"
 
 echo "src directory: $srcdir"
 echo "output directory: $dest"
+echo "board name: $boardname"
 echo
 echo "create output directory"
 mkdir "$dest" &&
+	mkdir "$dest/boot" &&
 	mkdir "$dest/bin" &&
-	mkdir "$dest/images" &&
-	mkdir "$dest/logs" || err_exit "create directory failed"
+	mkdir "$dest/images" || err_exit "create directory failed"
 
 echo "copy flash bin kernel and ramdisk"
 /bin/cp "$srcdir/uImage" "$dest/bin" &&
 	/bin/cp "$srcdir/uramdisk-flash.img" "$dest/bin/uramdisk.img" ||
 		err_exit "copy file failed"
 
-for i in $images; do
+for i in $flash_img; do
 	echo "copy file '$i' ..."
 	$CPCMD "$srcdir/$i" "$dest/images/$i" || err_exit "copy file failed"
 done
+
+echo "copy u-boot images ..."
+/bin/cp "$srcdir/$i" "$dest/boot/${boardname}_u-boot.img" || err_exit "copy u-boot failed"
 
 if [ -f "$srcdir/md5sum.txt" ]; then
 	echo "copy '$srcdir/md5sum.txt' to '$dest/images'"
@@ -104,6 +116,7 @@ else
 fi
 
 if [ "$initrel" = "1" ]; then
+	echo "create initlaize flag files"
 	touch "$dest/images/repartition"
 	touch "$dest/images/clearubootenv"
 fi
